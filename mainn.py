f666196aea6fbfdbed4009926ba84648081ad838
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2024-01-09 20:29:54 +0530

    Add descriptiom on the new columns

--- mainn.py
+++ mainn.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2  = st.columns(2)
 
 with col1:
     st.image("images (2)\\photo.png")
@@ -11,3 +11,6 @@ with col2:
     st.title("Ardit Sluce")
     content = """Thala vanakkam ellarum eppidi irukingae. Idhu thaa mass tappa tappa hello inss.."""
     st.info(content)
+
+content1 = """Below you can find some of the apps I have built in Python. Feel free to contact me. Vannakam ippo thala ma """
+st.write(content1)
